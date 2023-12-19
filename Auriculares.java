fcdec3172e8bc406c0f5f3360d38419b7d5ffdaa
lemzo <sanelamine2003@gmail.com>
2023-12-19 11:54:33 +0100

    commit inicial

---
+++ Auriculares.java
@@ -0,0 +1,11 @@
+public class Auriculares{
+    private int dbMax;
+    boolean cancelacionDeRuido;
+    
+    public Auriculares(){
+        dbMax = 0;
+        cancelacionDeRuido = false;
+    }
+    
+}
+
