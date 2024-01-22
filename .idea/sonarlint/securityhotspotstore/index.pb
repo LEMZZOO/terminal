cdfd744e0ef59a2a75e8a0c7f5c40841c616b3bd
agusbc31 <agusbc31@gmail.com>
2024-01-22 10:12:46 +0100

    AÑadiendo estructura y tres atributos a la primera clase

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,6 @@
 
 @
 Auriculares.java,d/f/dfa9559df6deeb77364d8652b7dd460b901f644a
+:
+
+Movil.java,8/2/8268abe10268e4397fb8fb5617d71fcf0285e663
---
+++ Movil.java
@@ -0,0 +1,9 @@
+public class Movil {
+    private String marcaMovil;
+    private int memoriaRAM;
+    private Auriculares misAuriculares;
+
+    public Movil(String marcaMovil, int memoriaRAM, Auriculares misAuriculares){
+
+    }
+}
